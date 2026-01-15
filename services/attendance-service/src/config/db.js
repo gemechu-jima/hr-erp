b26b90eb43688c99ea15b370a74112c97d5586ae
gemechu-jima <gemechujimacs@gmail.com>
2026-01-15 16:54:12 +0300

    udate into 127.0.0.1

--- services/attendance-service/src/config/db.js
+++ services/attendance-service/src/config/db.js
@@ -5,7 +5,7 @@ const mysql = require('mysql2/promise');
 
 async function ensureDatabase() {
   const connection = await mysql.createConnection({ 
-    host: 'localhost', 
+    host: '127.0.0.1', 
     user: 'root', 
     password: 'Mu@0!1Iy' 
   });
@@ -16,6 +16,7 @@ async function ensureDatabase() {
 const sequelize = new Sequelize('attendance_db', 'root', 'Mu@0!1Iy', {
   host: 'localhost',
   dialect: 'mysql',
+  port: 3306,
   logging: false, 
   pool: {
     max: 10,
